59e43005002ec9bf4240297d57c3925fb134f093
POBSIZ <gjehdtjr911@gmail.com>
2020-09-15 17:12:00 +0900

    SEC

--- ard.js
+++ ard.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var http = require('http');
 var xlsx = require('xlsx');
+var Excel = require('exceljs');
 var app = express();
 var path = require('path');
- 
-var book = xlsx.readFile('orgn.xlsx');
-var FWSname = book.SheetNames[0];
-var FWS = book.Sheets[FWSname];
-
 var server = http.createServer(app);
-server.listen(3000);
+server.listen(2393);
 
-var td = new Date();
- 
 // 시리얼 포트 설정
 // COM6 : 아두이노가 연결된 포트
 var serialPort  = require('serialport');
@@ -42,43 +36,84 @@ app.get('/',function(req,res) {
 var na = new Array();
 var nn = new Array();
 var nt = new Array();
+var td = new Date();
+var hsa = '0';
+
+
+var i = 1;
+var book = xlsx.readFile('ORGN.xlsx');
+var FWSname = book.SheetNames[0];
+var FWS = book.Sheets[FWSname];
+var ard_result;
+
+// let Cell_time = FWS['A'+i];
+// let Cell_switch = FWS['B'+i];
+
+// var workbook = new Excel.Workbook();
+// var worksheet = workbook.addWorksheet('test');
+// var Cell_time = worksheet.getCell('A'+i).value;
+// var Cell_switch = worksheet.getCell('B'+i).value;
 
 app.get('/test.ejs',function(req,res) {
     res.render(__dirname + '/views/test.ejs', {tad: na, taf: nn, tat: nt});
 });
 
-var hsa = '0';
-
 app.get('/controller/:id',function(req,res){
-    console.log(req.params.id);
     com3.write(req.params.id);
-    
-    let hours = td.getHours();
-
     hsa = req.params.id;
+    let hours = td.getHours();
+    let min = td.getMinutes();
+    var time = hours+':'+min;
 
-    if(hsa == 'On'){     
+    if(hsa == 'O'){     
         na.push(1);
         nn.push("On");
-        nt.push(hours);
-        // i = i+1;       
-    }else if(hsa == 'Off'){
+        nt.push(time);   
+    }else if(hsa == 'X'){
         na.push(0);
         nn.push("Off");
-        nt.push(hours);
+        nt.push(time);
+    }else if(hsa == 'L'){
+        na.push(111);  
+        nn.push("LCD");
+        nt.push(time);        
+    }else if(hsa == 'A'){ 
+        nn.push("A");
+        nt.push(time);
+    }else if(hsa == 'B'){ 
+        nn.push("B");
+        nt.push(time);
+    }else if(hsa == 'C'){ 
+        nn.push("C");
+        nt.push(time);
+    }else if(hsa == 'D'){ 
+        nn.push("D");
+        nt.push(time);
     }
-    console.log(na);
+
+    // console.log(na);
     console.log(nn);
     console.log(nt);
-    console.log(hsa);
+    // console.log(hsa);
+       
+    // for(i=i; i<nn.length; i++){
+    //     Cell_time = nt[i];
+    //     Cell_switch = nn[i];
+    // }
+    // workbook.xlsx.writeFile('export.xlsx');
+    
 
-    var ard_result = xlsx.utils.json_to_sheet([
-        {A: req.params.id, hours}
+    ard_result = xlsx.utils.json_to_sheet([
+        {A: req.params.id, time}
     ], {header: ['A'], skipHeader:true});
     ard_result["!cols"]=[{wpx: 10}];
-    xlsx.utils.book_append_sheet(book, ard_result, "ard_result" )
+
+    xlsx.utils.book_append_sheet(book, ard_result)//, FWSnam)
+    // xlsx.writeFile(book, 'export.xlsx');
     
     res.status(200).send('LED Controll OK!!');
 })
 
-xlsx.writeFile(book, 'export.xlsx');
+app.get('/saveExcel', function(req, res){
+    xlsx.writeFile(book, 'export.xlsx');
+});
--- arduinoNODE/arduinoNODE.ino
+++ arduinoNODE/arduinoNODE.ino
@@ -6,40 +6,55 @@
  */
  
  
-#define LED_01 11
-#define LED_02 12
-#define LED_03 13
- 
-#define LED_NUM 3
-unsigned int ledArray[] = { LED_01,LED_02, LED_03 } ;
+//#define LED_01 11
+//#define LED_02 12
+//#define LED_03 13
+//#define LED_NUM 3
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+
+//unsigned int ledArray[] = { LED_01,LED_02, LED_03 } ;
  
  
 void setup() {
   Serial.begin(9600);
- 
-  for(int i = 0 ; i < LED_NUM ; i++) {
-    pinMode(ledArray[i], OUTPUT) ;
-  }
+  lcd.begin(16, 2);
+    
+//  for(int i = 0 ; i < LED_NUM ; i++) {
+//    pinMode(ledArray[i], OUTPUT) ;
+//  }
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
  
 void loop() {
- 
+  lcd.setCursor(0,1);
+//  lcd.print("1234");
+
+  
   if(Serial.available()) {
       char c = Serial.read();
-//      Serial.write(c);
-      if(c == "On") {
-            for(int i = 0 ; i < LED_NUM ; i++) {
-                digitalWrite(ledArray[i], HIGH) ;
-            }
-           return;
+      Serial.write(c);
+      if(c == 'O') {
+            //for(int i = 0 ; i < LED_NUM ; i++) {
+            //    digitalWrite(ledArray[i], HIGH) ;
+            //}
+            pinMode(LED_BUILTIN,HIGH);
+            Serial.println(c);
+            lcd.print("A");
+           //return;
       }
-      if(c == "Off") {
-            for(int i = 0 ; i < LED_NUM ; i++) {
-                digitalWrite(ledArray[i], LOW) ;
-            }
-           return;
+      else if(c == 'X') {
+            //for(int i = 0 ; i < LED_NUM ; i++) {
+            //    digitalWrite(ledArray[i], LOW) ;
+            //}
+           
+            pinMode(LED_BUILTIN,LOW);
+            lcd.print("X");
+//          return;
       }
-      int ledNum = c - '1';
-      digitalWrite(ledArray[ledNum],!digitalRead(ledArray[ledNum])) ;
+      //int ledNum = c - '1';
+      //digitalWrite(ledArray[ledNum],!digitalRead(ledArray[ledNum])) ;
   }
 }
